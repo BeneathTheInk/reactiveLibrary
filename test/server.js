4e9b3e0fe5abb20df5e2f1af56528b6577b5404d
Tyler Johnson <tyler@vintyge.com>
2013-08-01 18:24:54 -0600

    new stuff works!

--- test/server.js
+++ test/server.js
@@ -6,6 +6,7 @@ var express = require("express"),
 var app = express();
 
 app.get("/bundle.js", function(req, res) {
+	res.type("application/javascript");
 	var b = browserify();
 	b.require(path.resolve(__dirname, "../lib/d.js"), { expose: "d" });
 	b.add(path.resolve(__dirname, "./test.js"));
